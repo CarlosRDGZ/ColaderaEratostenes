ced892a9eea0becd12789ff0cedddd8684ea817d
CarlosRDGZ <crodriguez20@ucol.mx>
2018-03-21 22:44:01 -0600

    Comentarios

--- Sieve.cs
+++ Sieve.cs
@@ -3,26 +3,27 @@ namespace Eratosthenes
     public class Sieve
     {
         private static int limit;
-        private static int[] numbers;
+        private static bool[] numbers;
 
         public static string FindPrimes(int limitNumber)
         {
             string num = "";
             int endl = 1;
             limit = limitNumber;
-            numbers = new int[limit];
+            numbers = new bool[limit];
+            
             for (int i = 0; i < limit; i++)
-                numbers[i] = 1;
+                numbers[i] = true;
             
             int prime = 2;
 
             while (prime > -1)
             {
                 int index = NextOne(prime);
-                while(index != -1)
+                while (index != -1)
                 {
                     if (index % prime == 0)
-                        numbers[index] = 0;
+                        numbers[index] = false;
                     index = NextOne(index);
                 }
                 num += (endl < 10) ? prime + ", " : prime + "," + System.Environment.NewLine;
@@ -33,10 +34,15 @@ namespace Eratosthenes
             return num;
         }
 
+        /*
+            Itera en el vector numbers, desde la posicion que se la pasa en el parametro mas
+            uno hasta el fin del vector y retorna la posicion del siguiente elmento cuyo valor
+            sea igual a 'true'. Si se llega al final y no se encontro otro 'true; retorna -1.
+         */
         private static int NextOne(int from)
         {
             for (int next = from + 1; next < limit; next++)
-                if (numbers[next] == 1)
+                if (numbers[next] == true)
                     return next;
             return -1;
         }
